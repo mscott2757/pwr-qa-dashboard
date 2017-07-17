630565ee45885ac82a61dc8c35fbb74610b9be10
Mason Scott <mscott2757@berkeley.edu>
2017-07-17 17:04:14 -0500

    notes for tests now also appear on app page

--- app/models/application_tag.rb
+++ app/models/application_tag.rb
@@ -3,11 +3,14 @@ class ApplicationTag < ApplicationRecord
 
   has_many :test_application_tags, dependent: :destroy
   has_many :tests, -> { distinct }, through: :test_application_tags
-  has_many :indirect_jira_tickets, through: :tests, source: :jira_tickets
-  has_many :notes, dependent: :destroy
-
   has_many :primary_tests, class_name: "Test", foreign_key: "primary_app_id"
+
+  has_many :indirect_jira_tickets, through: :tests, source: :jira_tickets
   has_many :primary_jira_tickets, through: :primary_tests, source: :jira_tickets
+
+  has_many :notes, dependent: :destroy
+  has_many :primary_notes, through: :primary_tests, source: :notes
+  has_many :indirect_notes, through: :tests, source: :notes
 
   def self.find_by_name(app_name)
     exists?(name: app_name) ? where(name: app_name).first : create(name: app_name)
@@ -108,4 +111,11 @@ class ApplicationTag < ApplicationRecord
     send(jira_method(method)).select { |ticket| ticket.test.env_tag == env_tag and !ticket.resolved }
   end
 
+  def notes_method(method)
+    method == "primary_tests" ? "primary_notes" : "indirect_notes"
+  end
+
+  def all_notes(method)
+    notes + send(notes_method(method))
+  end
 end
